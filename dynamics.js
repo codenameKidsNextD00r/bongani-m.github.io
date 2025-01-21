1e6749d885a59eb8bc08daa7a52985d3ed30dd38
Bongani Mlumbi <85309479+codenameKidsNextD00r@users.noreply.github.com>
2025-01-21 22:11:09 +0200

    Projects Section Update
    Updates to Projects section with additional navigation and additional images on index. Stylesheet made external now

---
+++ dynamics.js
@@ -0,0 +1,25 @@
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateClock, 1000);
+updateClock();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function scrollToSection(id){
+    document.getElementById(id).scrollIntoView({
+        behavior:"smooth"
+    });
+}
