77551b8f3ad2f3ba70448da20712d82603c8ca5f
Bongani Mlumbi <85309479+codenameKidsNextD00r@users.noreply.github.com>
2025-02-22 18:01:19 +0200

    responsiveness additions

--- dynamics.js
+++ dynamics.js
@@ -23,3 +23,8 @@ function scrollToSection(id){
         behavior:"smooth"
     });
 }
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+});
